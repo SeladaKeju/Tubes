50b998c732453c7668da9e4d27782f14108cd741
SeladaKeju <rizqiadit2430@gmail.com>
2023-12-21 23:53:54 +0700

    .

---
+++ daspro/main.py
@@ -0,0 +1,11 @@
+# File: main_program.py
+import tkinter as tk
+from kel3 import ProgramUI
+
+def main():
+    root = tk.Tk()
+    program_ui = ProgramUI(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
